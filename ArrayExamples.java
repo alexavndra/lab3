4459d66e4a077be5ea159385a259c2e1ee52cbe5
alex hernandez <88351332+alexavndra@users.noreply.github.com>
2023-10-24 13:26:37 -0700

    updated the tests and fixed the bugs in ArrayExamples.java

--- ArrayExamples.java
+++ ArrayExamples.java
@@ -1,22 +1,30 @@
-
-
 public class ArrayExamples {
 
   // Changes the input array to be in reversed order
+  // bug here
   static void reverseInPlace(int[] arr) {
+    int[] copiedArr = new int[arr.length];
+    
+    // makes sure copiedArr is its own array
+    for (int i = 0; i < arr.length; i += 1) {
+      copiedArr[i] = arr[i];
+    }
+
+    // reverses the array by taking the values from copiedArr and updated the original array
     for(int i = 0; i < arr.length; i += 1) {
-      arr[i] = arr[arr.length - i - 1];
+      arr[i] = copiedArr[copiedArr.length - 1 - i];
     }
   }
 
   // Returns a *new* array with all the elements of the input array in reversed
   // order
+  // bug here
   static int[] reversed(int[] arr) {
     int[] newArray = new int[arr.length];
     for(int i = 0; i < arr.length; i += 1) {
-      arr[i] = newArray[arr.length - i - 1];
+      newArray[i] = arr[arr.length - i - 1]; // reversed arr[i] = newArray[arr.length - i - 1] to newArray[i] = arr[arr.length - i - 1];
     }
-    return arr;
+    return newArray; // returns newArray instead of arr
   }
 
   // Averages the numbers in the array (takes the mean), but leaves out the
--- ArrayTests.java
+++ ArrayTests.java
@@ -2,17 +2,42 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 public class ArrayTests {
-	@Test 
+	@Test
 	public void testReverseInPlace() {
-    int[] input1 = { 3 };
+    int[] input1 = {3};
     ArrayExamples.reverseInPlace(input1);
-    assertArrayEquals(new int[]{ 3 }, input1);
+    assertArrayEquals(new int[]{3}, input1);
 	}
 
+  @Test // doesn't output correctly
+  public void testReverseInPlace2() {
+    int[] input1 = {3, 4, 5, 10, 2};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{2, 10, 5, 4, 3}, input1);
+  }
+
+  @Test
+	public void testReverseInPlace3() {
+    int[] input1 = {};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{}, input1);
+	}
 
   @Test
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  }
+
+  @Test // output is wrong
+  public void testReversed2() {
+    int[] input1 = {-100, -39, -4, 0, 1, 12, 20, 24, 30};
+    assertArrayEquals(new int[]{30, 24, 20, 12, 1, 0, -4, -39, -100}, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testAverageWithoutLowest() {
+    double[] input1 = {-10.39, -10.39850, -7, -5, -1, 0, 5, 10};
+    assertEquals(-1.19857142857, ArrayExamples.averageWithoutLowest(input1), 0.0001);
   }
 }
